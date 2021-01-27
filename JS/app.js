9809e4f146fd262cabd01a382bbd0a6ade562a9c
Charlie <chazadoc@gmail.com>
2021-01-27 17:24:29 +0000

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,11 @@
-const counter = document.querySelector('#count');
+const counter = document.querySelector('.count');
 const buttons = document.querySelectorAll('button');
 
 let count = 0;
 
-buttons.forEach(button => button.addEventListener('click', changeCount));
-function changeCount(e){
+buttons.forEach(button => button.addEventListener('click', () =>{
     e.target.classList.contains('decrease') ? count-- : 
     e.target.classList.contains('increase') ? count++ : count = 0;
         
     counter.textContent = count;
-}
+}));
